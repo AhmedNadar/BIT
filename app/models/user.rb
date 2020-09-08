eb0f7d91fd850a128245cb77f570352969ae46ce
Ahmed Nadar <ahmedfay@hotmail.com>
2020-09-08 12:11:38 +0400

    Add friendly_id to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,10 @@
 #
 
 class User < ApplicationRecord
-has_person_name
+  has_person_name
+
+  extend FriendlyId
+  friendly_id :name, use: :slugged
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_055648) do
+ActiveRecord::Schema.define(version: 2020_09_08_080645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +37,10 @@ ActiveRecord::Schema.define(version: 2020_09_08_055648) do
     t.boolean "admin", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
 end
